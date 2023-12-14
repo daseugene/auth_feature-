d9c9baaf288cfb5c2a3c2547f527df13d4f23c1f
Eugene Zabolotny <daseugene@MacBook-Air-Eugene.local>
2023-12-15 01:42:23 +0500

    add first steps to creating jwt

---
+++ setting.py
@@ -0,0 +1,20 @@
+from datetime import datetime, timedelta
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from jose import JWTError, jwt
+from passlib.context import CryptContext
+from pydantic import BaseModel
+
+
+SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+fake_user_db = {
+    "user": {
+        "username": "admin",
+        "password": "Tester2022",
+        "hashed_password": "$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW",
+        "disabled": False
+    }
+
+}
